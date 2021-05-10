efd0241569832bbc39d0857a85484c401a717f50
vishnusj <vishnusj12@gmail.com>
2021-05-10 13:12:27 +0530

    rps done w/o gui

---
+++ rps/script.js
@@ -0,0 +1,33 @@
+/* The Assignment is to build a rock paper scissors game. 
+
+The game has 2 players the user and the computer
+
+The game is comprised of 5 rounds, and the player who wins the first five rounds wins.
+
+Each round in the game, the user gets to choose rock,paper or sciccors.After the choice has been made by the user the computer chooses an option randomly. Win in each round is determined by choosing on object over the other. For example
+-------------------
+Options    | Matches   |   Win
+Rock         Scissors      Rock
+Paper        Rock          Paper
+Scissors     Paper         Scissor
+* And if both the players choose the same object and ends in a tie and round will not be count.
+
+Scissors   
+*/
+
+/* Psuedo Code 
+0.The round starts
+1.When the user select a value from three options
+2.The computer randomly select a value
+3.If user value > computer value, user wins a round
+    user round win count 1
+4.If user value< computer value, computer wins a round
+    computer round win count 1
+5.If user value=computer value, result in tie
+    round cancelled.
+6.Continue to the next round until a players round count reaches 5 round.The player who get there wins the game.
+7. Restart.
+
+the round should iterate upto until a player reaches a score of 5.
+*/
+
